004c4132fc2539cea6b47a0672a212ae57683d6e
Ben4444 <31416567+Ben4444@users.noreply.github.com>
2023-07-24 15:33:46 -0500

    Added a function to download Daymet data with input date.

--- Daymet.R
+++ Daymet.R
@@ -13,8 +13,8 @@
 library(daymetr)
 # NOTE - I'M USING THE DAYMETR PACKAGE, SPECIFICALLY DEVELOPED TO DOWNLOAD DAYMET DATA INTO R.
 # THIS RUNS THE RISK OF THE PACKAGE BECOMING OUTDATED THOUGH, MAYBE WE WANT TO SCRAPE DATA OFF DAYMET A DIFFERENT WAY.
-# WE WILL ALSO NEED TO MANUALLY CONVERT EVENT DATES INTO SEQUENTIAL YEAR DAYS (OR VICE-VERSA)
-# THIS PACKAGE ONLY ALLOWS YOU TO SPECIFY START AND END DATES ON THE YEAR LEVEL
+# WE WILL ALSO NEED TO MANUALLY CONVERT EVENT DATES INTO SEQUENTIAL YEAR DAYS (OR VICE-VERSA).
+# THIS PACKAGE ONLY ALLOWS YOU TO SPECIFY START AND END DATES ON THE YEAR LEVEL.
 
 # Downloading Daymet data for a specified longitude/latitude and time period
 daymet_data <- download_daymet(lat = 36.0133,
@@ -82,3 +82,57 @@ daymet_data <- row_to_names(daymet_data, row_number = 1)
 # Converting data to numeric
 daymet_data <- daymet_data |>
   mutate_if(is.character, as.numeric)
+
+
+# Writing a function to stream Daymet data from a URL
+# Function arguments:
+# Latitude as string
+lat <- ""
+# Longitude as string
+lon <- ""
+# Comma-separated Daymet variables of interest as string, a selection of: dayl,prcp,srad,swe,tmax,tmin,vp
+vars <- ""
+# Start Date as string, in format YYYY-MM-DD
+start <- ""
+# End Date as string, in format YYYY-MM-DD
+end <- ""
+
+daymet_download <- function(lat, lon, vars, start, end){
+  # Building URL
+  url <- str_glue('https://daymet.ornl.gov/single-pixel/api/data?',
+                  'lat=', lat,
+                  '&lon=', lon,
+                  '&vars=', vars,
+                  '&start=', start,
+                  '&end=', end,
+                  sep = "")
+  # Opening a streaming connection
+  connection <- curl(url)
+  open(connection)
+  # Streaming the data as text
+  output <- readLines(connection)
+  # Closing the streaming connection
+  close(connection)
+  # Pulling out the data of interest
+  output_length <- length(output)
+  data_string <- str_detect(output, "year,yday")
+  data_position <- match(TRUE, data_string)
+  tail_position <- output_length - data_position + 1
+  daymet_data <- tail(output, tail_position)
+  # Splitting the data strings
+  daymet_data <- str_split(daymet_data, ",", simplify = TRUE)
+  # Putting data into tibble
+  daymet_data <- as_tibble(daymet_data, .name_repair = 'unique')
+  daymet_data <- row_to_names(daymet_data, row_number = 1)
+  # Converting data to numeric
+  daymet_data <- daymet_data |>
+    mutate_if(is.character, as.numeric)
+}
+
+# Running the function with specified arguments
+lat <- "35.9621"
+lon <- "-84.2916"
+vars <- "tmax"
+start <- "2020-01-01"
+end <- "2020-01-02"
+daymet_data <- daymet_download(lat, lon, vars, start, end)
