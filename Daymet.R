a903c4cfa0b87915f07ae079dab39cfc4325ffb2
Ben4444 <31416567+Ben4444@users.noreply.github.com>
2023-08-23 12:14:07 -0500

    Added in trycatch for variable presence and type.

--- Daymet.R
+++ Daymet.R
@@ -23,24 +23,45 @@ library(tidyverse)
 library(terra)
 library(gtools)
 library(data.table)
+library(dht)
+
+# Greeting users
+greeting()
 
 # Writing functions
-# Creating function to import and process the patient addresses and optionally event dates
+# Creating function to import and process the input addresses and optionally event dates
 import_data <- function(.csv_filename = csv_filename, .year_start = year_start, .year_end = year_end) {
-  # Reading in the patient data
-  patient_data <- read_csv(.csv_filename)
-  # Assigning the very first column in the patient data as the ID column
-  id <- patient_data %>%
+  # Checking that the input data is a CSV file
+  if (!str_detect(.csv_filename, ".csv$")) {
+    stop(call. = FALSE, 'Input file must be a CSV.')
+  }
+  # Reading in the input data
+  input_data <- read_csv(.csv_filename)
+  # Assigning the very first column in the input data as the ID column
+  id <- input_data %>%
     select(1) %>%
     colnames()
-  # Filtering out rows in the patient data where lat or lon are missing
-  patient_data <- patient_data %>%
+  # Ensuring that numeric lat and lon are in the input data, and quitting if not
+  tryCatch({
+    check_for_column(input_data, column_name = "lat")
+    check_for_column(input_data, column_name = "lon")
+    check_for_column(input_data, column_name = "lat", column = input_data$lat, column_type = "numeric")
+    check_for_column(input_data, column_name = "lon", column = input_data$lon, column_type = "numeric")
+  }, error = function(e) {
+    print(e)
+    stop(call. = FALSE)
+  }, warning = function(w) {
+    print(w)
+    stop(call. = FALSE)
+  })
+  # Filtering out rows in the input data where lat or lon are missing
+  input_data <- input_data %>%
     filter(!is.na(lat) & !is.na(lon))
   # Separating the ID and address coordinates out into their own dataset
-  addresses <- patient_data %>%
+  addresses <- input_data %>%
     select(!!id, lat, lon)
   # Separating the ID and any other columns that aren't coordinates (presumed to be event dates) out into their own dataset
-  event_dates <- patient_data %>%
+  event_dates <- input_data %>%
     select(-lat, -lon)
   # If the ID column in addresses is equal to lat or lon (no ID column was supplied by the user) then creating an ID column in addresses and event_dates that is just the row number
   if (sum(addresses %>% select(!!id) == addresses %>% select(lat), na.rm = TRUE) > 0 | sum(addresses %>% select(!!id) == addresses %>% select(lon), na.rm = TRUE) > 0) {
@@ -54,12 +75,45 @@ import_data <- function(.csv_filename = csv_filename, .year_start = year_start, 
       select(id) %>%
       colnames()
   }
+  # Ensuring that the ID column is a character
+  tryCatch({
+    addresses <- addresses %>%
+      mutate(across(as.name(id), as.character))
+    event_dates <- event_dates %>%
+      mutate(across(as.name(id), as.character))
+  }, error = function(e) {
+    stop(call. = FALSE, 'Could not format ID variable as a string.')
+  }, warning = function(w) {
+    stop(call. = FALSE, 'Could not format ID variable as a string.')
+  })
   # If any columns that are not the ID column are characters, then converting them to dates
-  event_dates <- event_dates %>%
-    mutate(across(where(is.character) & !as.name(id), mdy))
+  tryCatch({
+    event_dates <- event_dates %>%
+      mutate(across(where(is.character) & !as.name(id), mdy))
+  }, error = function(e) {
+    print(e)
+  }, warning = function(w) {
+    stop(call. = FALSE, paste('Could not format user-supplied event dates as dates.',
+                              'Please provide event dates as YYYY-MM-DD or MM/DD/YYYY.'))
+  })
   # If the only column in the event_dates dataset is the ID column (no event dates were supplied by the user), then filling in every single day between year_start and year_end
-  year_start_date <- ymd(.year_start, truncated = 2L)
-  year_end_date <- ymd(.year_end, truncated = 2L)
+  if (.year_start > .year_end) {
+    stop(call. = FALSE, 'Please ensure that year_start is less than or equal to year_end.')
+  }
+  tryCatch({
+    year_start_date <- ymd(.year_start, truncated = 2L)
+  }, error = function(e) {
+    print(e)
+  }, warning = function(w) {
+    stop(call. = FALSE, 'Please provide year_start as a numeric four-digit year: YYYY.')
+  })
+  tryCatch({
+    year_end_date <- ymd(.year_end, truncated = 2L)
+  }, error = function(e) {
+    print(e)
+  }, warning = function(w) {
+    stop(call. = FALSE, 'Please provide year_end as a numeric four-digit year: YYYY.')
+  })
   if (leap_year(year_end_date)) {
     year_end_date <- year_end_date + 365
   } else {
@@ -72,19 +126,25 @@ import_data <- function(.csv_filename = csv_filename, .year_start = year_start, 
       mutate_all(as_date)
     event_dates <- cbind(event_dates, date_range)
   }
-  # Converting the patient addresses to a SpatVector
+  # Converting the input addresses to a SpatVector
   coords <- vect(addresses, geom = c("lon", "lat"), crs = "+proj=longlat +ellips=WGS84")
   # Returning a list of objects needed later
   out <- list("id" = id, "addresses" = addresses, "event_dates" = event_dates, "year_start_date" = year_start_date, "year_end_date" = year_end_date, "coords" = coords)
   return(out)
 }
 
-# Creating function to link the Daymet data coordinates to the patient address coordinates for a specified patient and specified date
+# Creating function to link the Daymet data coordinates to the input address coordinates for a specified ID and specified date
 daymet_select <- function(id_var, date_var, silent = FALSE, .template = template, .year_start_date = year_start_date, .year_end_date = year_end_date, .id = id, .time_dict = time_dict, .layer_dict = layer_dict, .daymet_data = daymet_data, .proj_coords = proj_coords) {
-  # Resetting the data to append with the main dataset template
+  # Resetting the data to append with the output data template
   to_append <- .template
   # If the specified date is beyond the Daymet data available, then breaking out of the function
-  date_var <- as_date(date_var)
+  tryCatch({
+    date_var <- as_date(date_var)
+  }, error = function(e) {
+    print(e)
+  }, warning = function(w) {
+    stop(call. = FALSE, 'Ensure that input date is formatted as YYYY-MM-DD.')
+  })
   if (date_var < .year_start_date | date_var > .year_end_date) {
     return(NA)
   }
@@ -93,7 +153,13 @@ daymet_select <- function(id_var, date_var, silent = FALSE, .template = template
     date_var <- date_var - 1
   }
   # Filling in the data to append with the ID and date
-  id_var <- as.character(id_var)
+  tryCatch({
+    id_var <- as.character(id_var)
+  }, error = function(e) {
+    stop(call. = FALSE, 'Could not format ID variable as a string.')
+  }, warning = function(w) {
+    stop(call. = FALSE, 'Could not format ID variable as a string.')
+  })
   to_append <- to_append %>%
     mutate(!!.id := !!id_var,
            date := !!date_var)
@@ -103,7 +169,7 @@ daymet_select <- function(id_var, date_var, silent = FALSE, .template = template
     filter(get(time_dict_col) == date_var) %>%
     select(number) %>%
     pull
-  # Extracting the Daymet data for the specified patient and specified date
+  # Extracting the Daymet data for the specified ID and specified date
   layers <- .layer_dict %>%
     filter(yr == year(date_var))
   daymet_extract <- function(dm_var, multiplier) {
@@ -129,15 +195,15 @@ daymet_select <- function(id_var, date_var, silent = FALSE, .template = template
       to_append <- rows_update(to_append, daymet_extract_output)
     }
   }, error = function(e) {
-    stop(call. = FALSE, paste('Error in Daymet data merge, user-supplied IDs must be unique.',
-                              id_var, 'is not unique. Quitting program.'))
+    stop(call. = FALSE, paste('Could not complete Daymet data merge, user-supplied IDs must be unique.',
+                              id_var, 'is not unique.'))
   }, warning = function(w) {
     print(w)
   })
   return(to_append)
 }
 
-# Importing and processing the patient data
+# Importing and processing the input data
 import_data_out <- import_data()
 id <- import_data_out$id
 addresses <- import_data_out$addresses
@@ -146,13 +212,13 @@ year_start_date <- import_data_out$year_start_date
 year_end_date <- import_data_out$year_end_date
 coords <- import_data_out$coords
 
-# Finding the min and max longitude and latitude out of all the patient address coordinates
+# Finding the min and max longitude and latitude out of all the input address coordinates
 min_lon <- min(addresses$lon)
 max_lon <- max(addresses$lon)
 min_lat <- min(addresses$lat)
 max_lat <- max(addresses$lat)
 
-# In order to preserve patient privacy, adding a random amount of noise to the bounding box of patient addresses
+# In order to preserve privacy, adding a random amount of noise to the bounding box of input addresses
 # Each bounding box point (e.g., maximum latitude) will be extended by an additional 1-22 kilometers
 noise <- runif(4, min = 0.01, max = 0.2)
 min_lon <- min_lon - noise[1]
@@ -161,7 +227,7 @@ min_lat <- min_lat - noise[3]
 max_lat <- max_lat + noise[4]
 
 # Downloading the Daymet NetCDF data defined by the coordinate bounding box: One file per variable per year
-# This procedure extracts the bounding box from the patient coordinates + noise, but we'll also want to add an option that lets the user specify their own bounding box
+# THIS PROCEDURE EXTRACTS THE BOUNDING BOX FROM THE INPUT COORDINATES + NOISE, BUT WE'LL ALSO WANT TO ADD AN OPTION THAT LETS THE USER SPECIFY THEIR OWN BOUNDING BOX
 # Additionally creating a template to link all Daymet data to - new observations will all be appended (vertical dataset)
 template_colnames <- c(id, "date")
 daymet_variables <- str_remove(daymet_variables, " ")
@@ -186,6 +252,10 @@ template <- template %>%
 
 # Loading the NetCDF files downloaded from Daymet as a SpatRaster raster stack
 netcdf_list <- list.files(pattern = "_ncss.nc$")
+# Checking for extra NetCDF files
+if (length(netcdf_list) > (length(seq(year_start, year_end)) * length(daymet_variables))) {
+  stop(call. = FALSE, 'Ensure that there are not extra NetCDF files in folder where Daymet data was downloaded to.')
+}
 time_dict <- tibble(number = 1:365)
 layer_dict_colnames <- c("dm_var", "yr", "multiplier")
 layer_dict <- as_tibble(matrix(nrow = length(netcdf_list), ncol = length(layer_dict_colnames)), .name_repair = ~ layer_dict_colnames)
@@ -213,7 +283,7 @@ for (i in 1:length(netcdf_list)) {
   }
 }
 
-# Changing the coordinate reference system of the patient addresses so they match that of Daymet
+# Changing the coordinate reference system of the input addresses so they match that of Daymet
 new_crs <- crs(daymet_data, proj = TRUE)
 proj_coords <- project(coords, new_crs)
 
@@ -227,10 +297,24 @@ event_dates <- event_dates %>%
   select(!!id, event_date_list)
 event_dates$event_date_list <- map(event_dates$event_date_list, unname)
 event_dates$event_date_list <- map(event_dates$event_date_list, na.omit)
-event_dates$event_date_list <- map(event_dates$event_date_list, as_date)
+tryCatch({
+  event_dates$event_date_list <- map(event_dates$event_date_list, as_date)
+}, error = function(e) {
+  print(e)
+}, warning = function(w) {
+  stop(call. = FALSE, 'Ensure that input date is formatted as YYYY-MM-DD.')
+})
 
 # For each event date list, adding in new dates that correspond to the specified lag
-lag <- as.numeric(lag)
+tryCatch({
+  if (!(lag %% 1 == 0) | is.logical(lag)) {
+    stop(call. = FALSE)
+  }
+}, error = function(e) {
+  stop(call. = FALSE, 'Ensure that user-supplied lag is a numeric whole number.')
+}, warning = function(w) {
+  print(w)
+})
 if (lag > 0) {
   for (lag_step in 1:lag) {
     new_dates <- map(event_dates$event_date_list, ~ .x - lag_step)
@@ -257,12 +341,12 @@ event_dates$id_list <- map(event_dates$event_date_list, as.character)
 tryCatch({
   event_dates$id_list <- map2(event_dates$id_list, unlist(unname(as.vector(event_dates[as.character(id)]))), str_replace, pattern = ".*")
 }, error = function(e) {
-  stop(call. = FALSE, 'Error in processing, user-supplied IDs are entirely missing. Quitting program.')
+  stop(call. = FALSE, 'User-supplied IDs are entirely missing.')
 }, warning = function(w) {
   print(w)
 })
 
-# Linking the Daymet data coordinates to the patient address coordinates across all event dates
+# Linking the Daymet data coordinates to the input address coordinates across all event dates
 daymet_select_output <- map2(unlist(event_dates$id_list), unlist(event_dates$event_date_list), daymet_select, silent = TRUE)
 daymet_select_output <- daymet_select_output[!is.na(daymet_select_output)]
 main_dataset <- rbindlist(daymet_select_output)
@@ -294,7 +378,5 @@ rm(list = ls(all.names = TRUE))
 unlink(list.files(pattern = "_ncss.nc$"), force = TRUE)
 
 # NEXT STEPS:
-# - ADD IN CODE TO TRYCATCH DATA TYPE CONVERSIONS (TELL USER THAT A CERTAIN CONVERSION FAILED, AND GIVE AN EXAMPLE OF EXPECTED INPUT)
-# - ADD IN CODE THAT CHECKS FOR LAT/LON IN INPUT DATA, AND THROWS AN ERROR IF IT'S NOT
 # - ADD IN CODE FOR EXTRA OPTIONS (DAYMETR OPTIONS, BOUNDING BOX OPTIONS)
 # - TEST WITH CAPRICORN DAYMET
